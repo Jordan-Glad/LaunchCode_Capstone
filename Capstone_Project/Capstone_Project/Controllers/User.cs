42e851748750db31b07250fcf51f3c678773459a
Scott-Weinberg <SCOTTWEINBERG81@GMAIL.COM>
2022-01-20 11:57:49 -0600

    created contolllers and view folders

---
+++ Capstone_Project/Capstone_Project/Controllers/User.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Capstone_Project.Controllers
+{
+    public class User : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
